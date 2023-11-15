59e33336a89c7514740ea32932bc7601c5b85472
jpedro85 <pedro.games30@gmail.com>
2023-11-15 20:04:35 +0000

    added scene

---
+++ Assets/Scripts/Inventory/GameItem.cs
@@ -0,0 +1,11 @@
+﻿using Scriptable_Objects.Items.Scripts;
+using UnityEngine;
+
+namespace Inventory
+{
+    // to be able to use the scriptable objects 
+    public class GameItem : MonoBehaviour
+    {
+        public ItemObject item;
+    }
+}
