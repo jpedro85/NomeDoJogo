00e6f7f9ab219dd537cfa066561165cee58003e2
jpedro85 <pedro.games30@gmail.com>
2023-12-11 23:21:20 +0000

    Fix: Fixed case where the load wasn't unsubscribed on onDisable Feat: Added sound manager DataPersistence

--- Assets/Scripts/HomeMenu/HomeScreenToLevel.cs
+++ Assets/Scripts/HomeMenu/HomeScreenToLevel.cs
@@ -5,6 +5,7 @@ public class HomeScreenToLevel : MonoBehaviour
 {
    public void PlayButton()
    {
+      DataPersistence.DataPersistenceManager.instance.saveGame();
       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
    }
 }
