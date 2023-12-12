4411eced48ef84afc3c2318a7dedd1de23fea933
ComaN10 <carlosmanuel2003@gmail.com>
2023-12-12 18:01:16 +0000

    All bugs were reviewed and  handled

--- Assets/scri.cs
+++ Assets/scri.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class SettingsPlayOpen : MonoBehaviour
 {
-    public void SettingsButton()
+    public void SettingsBtn()
     {
         DataPersistence.DataPersistenceManager.instance.saveGame();
         SceneManager.LoadScene(4);
